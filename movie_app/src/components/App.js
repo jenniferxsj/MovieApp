2f9a2366dec22c5815c4403da0567eb67a6db32f
Shujun Xiao <xiao.57578@gmail.com>
2023-01-13 19:15:26 -0800

    change style

--- movie_app/src/components/App.js
+++ movie_app/src/components/App.js
@@ -78,7 +78,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <Header text="movieApp"/>
+      <Header text="Movie Search App"/>
       <Search search={search}/>
       <p className = "App-intro">Sharing a few of favorite movies</p>
       <div className="movies">
